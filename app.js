04f02f20bfe23686e0cedf2859ae2fe2d919b612
Andrew Kish <akish-github@kishcom.com>
2013-09-18 18:08:12 -0400

    Port is an env variable now

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: "http://" + domain + ":8888/auth/github/callback"
+    callbackURL: "http://" + domain + "/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     var profileToken = "githubId" + profile.id;
